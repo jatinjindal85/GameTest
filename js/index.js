b9dd99aa53184479413a68706ffe366d5c0488cc
Mudit Goel <muditgoel01@gmail.com>
2014-06-16 23:56:13 -0700

    placeholder controllers for get and post

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 
-angular.module('Zipcar', [])
-.controller('NavCtrl', function($scope){
+var app = angular.module('Zipcar', []);
+
+
+app.controller('GetCtrl', function($scope){
+	$scope.games = [
+		{name:'Age Of Empires', image:'image1.png', owner:'John', distance:'1.5'},
+		{name:'Roller Coaster Tycoon', image:'image2.png', owner:'Alex', distance:'2.5'},
+		{name:'Prince of Persia', image:'image3.jpg', owner:'Crazy', distance:'0.5'},
+		{name:'Midtown Madness', image:'image4.jpg', owner:'John', distance:'7'}
+	];
+
+});
+
+app.controller('PostCtrl', function($scope){
+	$scope.newGame = {name:'Name', image:'default.jpg'};
+});
+
+
+app.controller('NavCtrl', function($scope){
 	$scope.slide = 1;
 	$scope.setSlide = function(slideNum){
 		$scope.slide = slideNum;
