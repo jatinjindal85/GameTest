fd2cf4b700766307d0df03a0326719facf3e0cd1
Mudit Goel <muditgoel01@gmail.com>
2014-06-18 21:01:12 -0700

    basic infrastructure for UI and controllers.
    add 4 controllers (nag, find, posted, rented). add tabs and lists to UI.

--- js/index.js
+++ js/index.js
@@ -1,37 +1,61 @@
 
 var app = angular.module('Zipcar', []);
 
-
-app.controller('GetCtrl', function($scope){
-	$scope.games = [
-		{name:'Age Of Empires', image:'image1.png', owner:'John', distance:'1.5'},
-		{name:'Roller Coaster Tycoon', image:'image2.png', owner:'Alex', distance:'2.5'},
-		{name:'Prince of Persia', image:'image3.jpg', owner:'Crazy', distance:'0.5'},
-		{name:'Midtown Madness', image:'image4.jpg', owner:'John', distance:'7'}
-	];
+app.controller('NavCtrl', function($scope){
+	$scope.tab = 'find';
+	$scope.setTab = function(tabName){
+		$scope.tab = tabName;
+	}
 });
 
-app.controller('PostCtrl', function($scope){
 
+app.controller('FindCtrl', function($scope){
 	$scope.games = [
 		{name:'Age Of Empires', image:'image1.png', owner:'John', distance:'1.5'},
 		{name:'Roller Coaster Tycoon', image:'image2.png', owner:'Alex', distance:'2.5'},
 		{name:'Prince of Persia', image:'image3.jpg', owner:'Crazy', distance:'0.5'},
 		{name:'Midtown Madness', image:'image4.jpg', owner:'John', distance:'7'}
 	];
+	$scope.getResultsCount = function(){
+		return $scope.games.length;
+	};
+	$scope.requestGame = function(){
+		alert("Requesting game.");
+		//$scope.games.push({name:$scope.newGameName, image:$scope.newGamePic, owner:'Gulla', distance:'2.2'});
+		//$scope.newGameName = '';
+	};
+});
 
+app.controller('PostedCtrl', function($scope){
+	$scope.games = [
+		{name:'Age Of Empires', image:'image1.png', location:'Sunnyvale, CA', views:'2', requests:'0'},
+		{name:'Prince of Persia', image:'image2.png', location:'Sunnyvale, CA', views:'5', requests:'1'}
+	];
 	$scope.addGame = function(){
-		$scope.games.push({name:$scope.newGameName, image:$scope.newGamePic, owner:'Gulla', distance:'2.2'});
+		alert("Adding game.");
+		$scope.games.push({name:$scope.newGameName, image:$scope.newGamePic, location:'Sunnyvale, CA', views:'0', requests:'0'});
 		$scope.newGameName = '';
 	};
+});
 
-	//$scope.newGame = {name:'Name', image:'default.jpg'};
+app.controller('RentedCtrl', function($scope){
+	$scope.rentedtab = 'owned';
+	$scope.setRentedTab = function(rentedTabName){
+		$scope.rentedtab = rentedTabName;
+	}
+	$scope.games = [
+		{name:'Age Of Empires', image:'image1.png', location:'Sunnyvale, CA', views:'2', requests:'0'},
+		{name:'Prince of Persia', image:'image2.png', location:'Sunnyvale, CA', views:'5', requests:'1'}
+	];
 });
 
 
-app.controller('NavCtrl', function($scope){
+
+
+/*app.controller('NavCtrl', function($scope){
 	$scope.slide = 1;
 	$scope.setSlide = function(slideNum){
 		$scope.slide = slideNum;
 	}
 });
+*/
